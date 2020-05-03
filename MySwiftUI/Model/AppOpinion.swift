b98ebba29dd7b4f2abd0575d4eedc779a3978864
Sengsathit <sengsathit@gmail.com>
2020-05-03 18:33:32 +0200

    .

---
+++ MySwiftUI/Model/AppOpinion.swift
@@ -0,0 +1,13 @@
+//
+//  AppOpinion.swift
+//  MySwiftUI
+//
+//  Created by Sengsathit on 02/05/2020.
+//  Copyright © 2020 Sengsathit. All rights reserved.
+//
+
+import Foundation
+
+class OpinionCounter: ObservableObject {
+    @Published var globalCounter: Int = 0
+}
